f7676c849f80afcbc86b810c9480bf9d048aa6b5
Sergey Krasavin <krasszen@ya.ru>
2022-10-16 23:22:46 +0500

    add comments task 4.5

--- src/main/kotlin/lesson_4.5.kt
+++ src/main/kotlin/lesson_4.5.kt
@@ -8,5 +8,5 @@ fun decryptedString (encryptString : String) : String {
     val twoHalfString: String = encryptString.removeRange(0, twoHalf).reversed().map { char -> char - 4}.joinToString("").replace("_", " ")
     val oneHalfString: String = encryptString.removeRange(twoHalf, encryptString.length).map { char -> char + 1}.joinToString("").replace("5", "s").replace("4","u").map { char -> char - 3}.joinToString("").replace("0", "o")
     val finalString = oneHalfString + twoHalfString
-    return finalString
+    return finalString // Don't worry. Be happy :)
 }
